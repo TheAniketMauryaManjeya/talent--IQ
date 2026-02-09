97424e0c0741c7764043a98913e8e1fe1a199ffc
TheAniketMauryaManjeya <mauryavanshi72@gmail.com>
2026-02-10 02:03:01 +0530

    dashboard page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react';
+import { useUser } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 
